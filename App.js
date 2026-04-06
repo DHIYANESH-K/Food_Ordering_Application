bf281dd6ce8b21464ef9f1eb6aa800be01dd008c
Dhiyanesh-K <dhiyanesh177@gmail.com>
2026-04-06 12:41:59 +0530

    Configure Parcel bundler, add browserslist, and update scripts in package.json

--- App.js
+++ App.js
@@ -1,48 +1,5 @@
-// Create structure like this : 
-
-{/* <div id="root">
-    <div id="child">
-        <h1 class="heading">Hello world</h1>
-    </div>
-</div> */}
-
-
-// let object=React.createElement("div",{id:"child"},
-//     React.createElement("h1",{class:"heading"},"Hello world"));
-// let root=ReactDOM.createRoot(document.getElementById("root"));
-// root.render(object);
-
-
-
-
-{/* <div id="root">
-    <div id="child">
-        <h1 class="heading_1">Hello world</h1>
-        <h2 class="heading_2">Hello everyone</h2>
-    </div>
-</div> */}
-
-// let object=React.createElement("div",{id:"child"},[
-//     React.createElement("h1",{className:"heading_1"},"Hello world"),
-//     React.createElement("h2",{className:"heading_2"},"Hello everyone")
-// ]);
-// console.log(object)
-// let root=ReactDOM.createRoot(document.getElementById("root"));
-// root.render(object);
-
-
-
-
-{/* <div id="parent">
-    <div id="child_1">
-        <h1 class="heading_1">Hello world</h1>
-        <h2 class="heading_2">Hello everyone</h2>
-    </div>
-    <div id="child_2">
-        <h1 class="heading_21">Hello world</h1>
-        <h2 class="heading_22">Hello everyone</h2>
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 let object=React.createElement("div",{id:"parent"},[
@@ -51,8 +8,8 @@ let object=React.createElement("div",{id:"parent"},[
         React.createElement("h2",{className:"heading_2"},"Hello everyone")
     ]),
     React.createElement("div",{id:"child_2"},[
-        React.createElement("h1",{className:"heading_21"},"Hello world"),
-        React.createElement("h2",{className:"heading_22"},"Hello everyone")
+        React.createElement("h1",{className:"heading_21"},"Hello-world"),
+        React.createElement("h2",{className:"heading_22"},"Hello-everyone")
     ])
 ]);
 console.log(object)
