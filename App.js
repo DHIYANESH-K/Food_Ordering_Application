72044603db8a2233a788286431fb50f3055d1b6e
Dhiyanesh-K <dhiyanesh177@gmail.com>
2026-04-06 13:13:54 +0530

    Add JSX code

--- App.js
+++ App.js
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-let object=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child_1"},[
-        React.createElement("h1",{className:"heading_1"},"Hello world"),
-        React.createElement("h2",{className:"heading_2"},"Hello everyone")
-    ]),
-    React.createElement("div",{id:"child_2"},[
-        React.createElement("h1",{className:"heading_21"},"Hello-world"),
-        React.createElement("h2",{className:"heading_22"},"Hello-everyone")
-    ])
-]);
-console.log(object)
+// JSX - HTML-like or XML-like syntax
+const jsxHeading=(<h1 className="heading">
+    Hello World
+    </h1>)
+
 let root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(object);
+root.render(jsxHeading);
