1a68a01a4548e7fd21d175dee641f9e0bf777789
Dhiyanesh-K <dhiyanesh177@gmail.com>
2026-04-06 15:47:55 +0530

    Create Header and Body components

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "url:./assets/logo.png";
 
-const HeadingComponent=()=>{
-    return <h1 className="heading">React Functional Component</h1>;
+const Header = () => {
+    console.log(logo)
+    return (<div className="header">
+        <div className="logo_container">
+            <img className="logo" src={logo} alt="logo" />
+        </div>
+        <div className="nav_item">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    );
+};
+
+let styleCard={
+    backgroundColor:"lightgray",
+};
+
+const RestaurantCard=()=>{
+    return(<div className="res_card" style={styleCard}>
+        <h3>Restaurant</h3>
+    </div>)
 }
 
-const heading=(<h1 className="heading">
-    Hello World
-    {HeadingComponent()}
-    <HeadingComponent/>
-    <HeadingComponent></HeadingComponent>
-    </h1>);
+const Body=()=>{
+    return (<div>
+        <div className="search">Search</div>
+        <div className="res_container">
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+            <RestaurantCard/>
+        </div>
+    </div>)
+}
 
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body/>
+        </div>
+    );
+};
 
-let root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
