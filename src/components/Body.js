e887f66580fe83139fb2b3507ebc08e8c29b2fd0
Dhiyanesh-K <dhiyanesh177@gmail.com>
2026-04-13 17:14:29 +0530

    Add search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,32 +4,45 @@ import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-    let [listOfRestaurants,setListOfRestaurants]=useState([]);
+    let [listOfRestaurants, setListOfRestaurants] = useState([]);
+    let [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    let [searchText, setSearchText] = useState("")
 
-    let getData=()=>{
+    let getData = () => {
         setListOfRestaurants(resData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setFilteredRestaurants(resData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
     }
-    
-    useEffect(()=>{
-        setTimeout(()=>getData(),2000);
-    },[])
 
-    if(listOfRestaurants.length===0)
-    {
-        return(<Shimmer/>)
+    useEffect(() => {
+        setTimeout(() => getData(), 2000);
+    }, [])
+
+    if (listOfRestaurants.length === 0) {
+        return (<Shimmer />)
     }
 
     return (<div>
-        <button className="search"
-        onClick={()=>{
-            const filteredList=listOfRestaurants.filter(
-                (res)=>res.info.avgRating>=4.5
-            );
-            setListOfRestaurants(filteredList);
-        }}>Top Rated Restaurants</button>
+        <div className="filter">
+            <input className="search_input" type="text" value={searchText} onChange={(e) => {
+                setSearchText(e.target.value)
+            }}></input>
+            <button className="search_btn" onClick={() => {
+                let filteredRes = listOfRestaurants.filter((element, index) => {
+                    return element.info.name.toLowerCase().includes(searchText.toLowerCase())
+                })
+                setFilteredRestaurants(filteredRes);
+            }}>Search</button>
+            <button className="search"
+                onClick={() => {
+                    const filteredList = listOfRestaurants.filter(
+                        (res) => res.info.avgRating >= 4.5
+                    );
+                    setFilteredRestaurants(filteredList);
+                }}>Top Rated Restaurants</button>
+        </div>
         <div className="res_container">
             {
-                listOfRestaurants.map((element)=>(<RestaurantCard key={element.info.id} resData={element}/>))
+                filteredRestaurants.map((element) => (<RestaurantCard key={element.info.id} resData={element} />))
             }
         </div>
     </div>)
