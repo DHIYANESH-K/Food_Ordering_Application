2b1777c48ee0b0bf9ea3ba416101987fccae9585
Dhiyanesh-K <dhiyanesh177@gmail.com>
2026-05-13 15:08:31 +0530

    created state variable inside class-based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,6 +4,12 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props);
+
+        this.state={
+            count1:0,
+            count2:10,
+            count3:100
+        }
     }
 
     render(){
@@ -11,6 +17,15 @@ class UserClass extends React.Component{
         let {name, location, phone} = this.props;
 
         return(<div>
+            <div>Count 1 : {this.state.count1}</div>
+            <div>Count2 : {this.state.count2}</div>
+            <div>Count3 : {this.state.count3}</div>
+            <button onClick={()=>{
+                this.setState({
+                    count1: this.state.count1 + 1,
+                    count2: this.state.count2 - 1
+                });
+            }}>Increment</button>
             <div>Name : {name}</div>
             <div>Location : {location}</div>
             <div>Contact : {phone}</div>
